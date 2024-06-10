9a5866b074694058c0d57e8582be9b5f2a2e0657
Nick Duskey <nick@roveiq.com>
2024-06-10 19:41:31 -0400

    Add shadcn-svelte and components

---
+++ src/lib/components/nav/MainNav.svelte
@@ -0,0 +1,6 @@
+<nav class="mx-6 flex items-center space-x-4 lg:space-x-6">
+	<a href="/foo" class="text-sm font-medium transition-colors hover:text-primary"> Overview </a>
+	<a href="/foo" class="text-sm font-medium transition-colors hover:text-primary"> Customers </a>
+	<a href="/foo" class="text-sm font-medium transition-colors hover:text-primary"> Products </a>
+	<a href="/foo" class="text-sm font-medium transition-colors hover:text-primary"> Settings </a>
+</nav>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,13 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import Logo from '$lib/components/Logo.svelte';
+	import MainNav from '$lib/components/nav/MainNav.svelte';
+</script>
+
+<div class="flex-col md:flex">
+	<div class="border-b">
+		<div class="flex h-16 items-center px-4">
+			<Logo />
+			<MainNav />
+		</div>
+	</div>
+</div>
